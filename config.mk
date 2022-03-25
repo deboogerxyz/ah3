72d610156a4898a0b50fe1e804745e656c7960d4
debooger <debooger@debooger.xyz>
2022-03-25 12:44:37 +0100

    Optimize for size with -Os in config.mk

--- config.mk
+++ config.mk
@@ -3,7 +3,7 @@ LIBS = -lpthread
 
 # Flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_GNU_SOURCE -D_POSIX_C_SOURCE=200809L
-CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -O0 -fPIC ${CPPFLAGS}
+CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os -fPIC ${CPPFLAGS}
 LDFLAGS  = -shared ${LIBS}
 
 # Compiler and linker
