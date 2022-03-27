53e80cca0b3c3e005d9fe2b3a97408c21173accb
debooger <debooger@debooger.xyz>
2022-03-27 01:14:06 +0100

    Prettified code in mem.c

--- mem.c
+++ mem.c
@@ -33,14 +33,12 @@ callback(struct dl_phdr_info *info, size_t size, void *addr)
 static Lib *
 getlibinfo(const char *name)
 {
-	Lib *it;
-
 	if (cvector_empty(libs)) {
 		cvector_grow(libs, 150);
 		dl_iterate_phdr(callback, NULL);
 	}
 
-	for (it = cvector_begin(libs); it != cvector_end(libs); it++) {
+	for (Lib *it = cvector_begin(libs); it != cvector_end(libs); it++) {
 		if (!strstr(it->name, name))
 			continue;
 		return it;
@@ -52,18 +50,14 @@ getlibinfo(const char *name)
 static uintptr_t
 find(const char *libname, const char *ptrn)
 {
-	Lib *lib;
-	const char *start, *end,
-	           *first, *second;
-
-	lib = getlibinfo(libname);
+	Lib *lib = getlibinfo(libname);
 	if (!lib || !lib->addr || !lib->size)
 		return 0;
 
-	start = (const char *)lib->addr;
-	end   = start + lib->size;
-	first = start;
-	second = ptrn;
+	const char *start  = (const char *)lib->addr;
+	const char *end    = start + lib->size;
+	const char *first  = start;
+	const char *second = ptrn;
 
 	while (first < end && *second)
 		if (*first == *second || *second == '?') {
@@ -89,17 +83,15 @@ reltoabs(uintptr_t addr)
 void
 mem_init(void)
 {
-	void *tier0;
-
 	mem = calloc(1, sizeof(Mem));
 
-	tier0 = dlopen("libtier0_client.so", RTLD_NOLOAD | RTLD_NOW);
+	void *tier0 = dlopen("libtier0_client.so", RTLD_NOLOAD | RTLD_NOW);
 	*(void **)&mem->debugmsg = dlsym(tier0, "Msg");
 	dlclose(tier0);
 
 	*(void **)&mem->isotherenemy = (void *)reltoabs(find("csgo/bin/linux64/client_client.so", "\xE8????\x84\xC0\x44\x89\xE2") + 1);
-	*(void **)&mem->clientmode = *(void **)reltoabs(reltoabs(VMT(intf->client)[10] + 12) + 4);
-	*(void **)&mem->gvars = *(void **)reltoabs(VMT(intf->client)[11] + 16);
+	*(void **)&mem->clientmode   = *(void **)reltoabs(reltoabs(VMT(intf->client)[10] + 12) + 4);
+	*(void **)&mem->gvars        = *(void **)reltoabs(VMT(intf->client)[11] + 16);
 	*(void **)&mem->setabsorigin = (void *)reltoabs(find("csgo/bin/linux64/client_client.so", "\xE8????\x49\x8B\x07\x31\xF6") + 1);
 }
 
