5e47f74a2383acae7012b0e6df1de3510e72e59e
debooger <debooger@debooger.xyz>
2022-03-27 01:11:02 +0100

    Prettified code in intf.c

--- intf.c
+++ intf.c
@@ -9,12 +9,10 @@ static uintptr_t *
 find(const char *libname, const char *name)
 {
 	uintptr_t *(*create)(const char *, int *);
-	uintptr_t *found;
 
-	*(void **)&create = dlsym(dlopen(libname, RTLD_NOLOAD | RTLD_LAZY),
-	                          "CreateInterface");
+	*(void **)&create = dlsym(dlopen(libname, RTLD_NOLOAD | RTLD_LAZY), "CreateInterface");
 	if (create) {
-		found = create(name, 0);
+		uintptr_t *found = create(name, 0);
 		if (found)
 			return found;
 	}
@@ -27,12 +25,10 @@ intf_init(void)
 {
 	intf = calloc(1, sizeof(Intf));
 
-	intf->client  = find("csgo/bin/linux64/client_client.so",
-	                     "VClient018");
+	intf->client  = find("csgo/bin/linux64/client_client.so", "VClient018");
 	intf->cvar    = find("libvstdlib_client.so", "VEngineCvar007");
 	intf->engine  = find("engine_client.so", "VEngineClient014");
-	intf->entlist = find("csgo/bin/linux64/client_client.so",
-	                     "VClientEntityList003");
+	intf->entlist = find("csgo/bin/linux64/client_client.so", "VClientEntityList003");
 }
 
 void
