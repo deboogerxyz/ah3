f93714f5f9b9eda535c27815b9092c9a105dc0ba
debooger <debooger@debooger.xyz>
2022-03-27 01:52:36 +0100

    Added whitespace to util.h

--- util.h
+++ util.h
@@ -5,9 +5,11 @@
 
 #define VMT(X) (*((uintptr_t **)(X)))
 #define VFN(type, vmt, i) ((type)(vmt)[i])
+
 #define MAX(A, B) ((A) > (B) ? (A) : (B))
 #define MIN(A, B) ((A) < (B) ? (A) : (B))
 #define CLAMP(X, A, B) (MIN((B), MAX((X), (A))))
+
 #define CAT(A, B) A##B
 #define PADNAME(X) CAT(pad, X)
 #define PAD(X) char PADNAME(__LINE__)[X]
