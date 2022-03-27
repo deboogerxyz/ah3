bfe2d6899399f6d2d845f246cdad66771b9b4a3d
debooger <debooger@debooger.xyz>
2022-03-27 01:30:44 +0100

    Prettified code in util.c

--- util.c
+++ util.c
@@ -5,12 +5,12 @@
 unsigned int
 hash(const char *data)
 {
-	int i;
 	unsigned int hashed = 0x811C9DC5;
 
-	for (i = 0; i < strlen(data); i++) {
+	for (int i = 0; i < strlen(data); i++) {
 		hashed ^= data[i];
 		hashed *= 0x1000193;
 	}
+
 	return hashed;
 }
