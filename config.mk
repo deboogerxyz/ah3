367a4c90a78f271e7163ccc1f07a432723ceba7b
debooger <debooger@debooger.xyz>
2022-04-01 21:02:58 +0200

    Disabled leftover debugging code

--- config.mk
+++ config.mk
@@ -3,7 +3,7 @@ LIBS = -lcjson -lGL -lGLEW -lm -lpthread -lSDL2
 
 # Flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_GNU_SOURCE -D_POSIX_C_SOURCE=200809L
-CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -O2 -fPIC ${CPPFLAGS} -O0 -g
+CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -O2 -fPIC ${CPPFLAGS}
 LDFLAGS  = -shared ${LIBS}
 
 # Compiler and linker
